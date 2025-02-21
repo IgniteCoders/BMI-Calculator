ef446e1cb17574afa82ce27f3f49b9c273e019bc
IgniteCoders <fernando.paniagua.javascript@gmail.com>
2025-02-21 13:23:26 +0100

    We calculate the bmi and show the result

--- app/src/main/java/com/example/bmi_calculator/MainActivity.kt
+++ app/src/main/java/com/example/bmi_calculator/MainActivity.kt
@@ -3,16 +3,19 @@ package com.example.bmi_calculator
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var heightEditText: EditText
     lateinit var weightEditText: EditText
     lateinit var calculateButton: Button
+    lateinit var resultTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,13 +33,18 @@ class MainActivity : AppCompatActivity() {
         heightEditText = findViewById(R.id.heightEditText)
         weightEditText = findViewById(R.id.weightEditText)
         calculateButton = findViewById(R.id.calculateButton)
+        resultTextView = findViewById(R.id.resultTextView)
 
         heightEditText.setText("180")
         weightEditText.setText("70")
 
         calculateButton.setOnClickListener {
-            println("Altura: " + heightEditText.text.toString())
-            println("Peso: " + weightEditText.text.toString())
+            val height = heightEditText.text.toString().toFloat()
+            val weight = weightEditText.text.toString().toFloat()
+
+            val result = weight / (height / 100).pow(2)
+
+            resultTextView.text = "$result"
         }
     }
 
