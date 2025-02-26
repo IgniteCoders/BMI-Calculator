13cf5c9de5a4472aae6bef1e3ed6a74f921b723d
IgniteCoders <theboom.kajas@gmail.com>
2025-02-26 13:04:17 +0100

    Added an obesity alert for health risk. Added French strings.

--- app/src/main/java/com/example/bmi_calculator/MainActivity.kt
+++ app/src/main/java/com/example/bmi_calculator/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.bmi_calculator
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.slider.Slider
 import java.util.Locale
 import kotlin.math.pow
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -41,6 +45,76 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        initViews()
+
+        heightSlider.addOnChangeListener { slider, value, fromUser ->
+            height = value
+            heightTextView.text = "${height.toInt()}"
+        }
+
+        weightAddButton.setOnClickListener {
+            weight ++
+            if (weight > 300) {
+                weight = 300f
+            }
+            weightTextView.text = "${weight.toInt()}"
+        }
+
+        weightMinusButton.setOnClickListener {
+            weight --
+            if (weight < 1) {
+                weight = 1f
+            }
+            weightTextView.text = "${weight.toInt()}"
+        }
+
+        calculateButton.setOnClickListener {
+            calculateBMI()
+        }
+    }
+
+    private fun calculateBMI() {
+        val result = weight / (height / 100).pow(2)
+
+        resultTextView.text = String.format(Locale.getDefault(), "%.2f", result)
+
+        var colorId = 0
+        var textId = 0
+        when (result) {
+            in 0f..<18.5f -> {
+                colorId = R.color.imc_underweight
+                textId = R.string.imc_underweight
+            }
+            in 18.5f..<25f -> {
+                colorId = R.color.imc_normal
+                textId = R.string.imc_normal
+            }
+            in 25f..<30f -> {
+                colorId = R.color.imc_overweight
+                textId = R.string.imc_overweight
+            }
+            in 30f..<35f -> {
+                colorId = R.color.imc_obesity1
+                textId = R.string.imc_obesity1
+                showObesityDialog()
+            }
+            in 35f..<40f -> {
+                colorId = R.color.imc_obesity2
+                textId = R.string.imc_obesity2
+                showObesityDialog()
+            }
+            else -> {
+                colorId = R.color.imc_obesity3
+                textId = R.string.imc_obesity3
+                showObesityDialog()
+            }
+        }
+        resultTextView.setTextColor(getColor(colorId))
+        descriptionTextView.setTextColor(getColor(colorId))
+        descriptionTextView.text = getString(textId)
+    }
+
+    fun initViews() {
         heightTextView = findViewById(R.id.heightTextView)
         heightSlider = findViewById(R.id.heightSlider)
 
@@ -51,58 +125,17 @@ class MainActivity : AppCompatActivity() {
         calculateButton = findViewById(R.id.calculateButton)
         resultTextView = findViewById(R.id.resultTextView)
         descriptionTextView = findViewById(R.id.descriptionTextView)
+    }
 
-        heightSlider.addOnChangeListener { slider, value, fromUser ->
-            height = value
-            heightTextView.text = "${height.toInt()}"
-        }
-
-        weightAddButton.setOnClickListener {
-            weight ++
-            weightTextView.text = "${weight.toInt()}"
-        }
-
-        weightMinusButton.setOnClickListener {
-            weight --
-            weightTextView.text = "${weight.toInt()}"
-        }
-
-        calculateButton.setOnClickListener {
-            val result = weight / (height / 100).pow(2)
-
-            resultTextView.text = String.format(Locale.getDefault(), "%.2f", result)
-
-            var colorId = 0
-            var textId = 0
-            when (result) {
-                in 0f..<18.5f -> {
-                    colorId = R.color.imc_underweight
-                    textId = R.string.imc_underweight
-                }
-                in 18.5f..<25f -> {
-                    colorId = R.color.imc_normal
-                    textId = R.string.imc_normal
-                }
-                in 25f..<30f -> {
-                    colorId = R.color.imc_overweight
-                    textId = R.string.imc_overweight
-                }
-                in 30f..<35f -> {
-                    colorId = R.color.imc_obesity1
-                    textId = R.string.imc_obesity1
-                }
-                in 35f..<40f -> {
-                    colorId = R.color.imc_obesity2
-                    textId = R.string.imc_obesity2
-                }
-                else -> {
-                    colorId = R.color.imc_obesity3
-                    textId = R.string.imc_obesity3
-                }
-            }
-            resultTextView.setTextColor(getColor(colorId))
-            descriptionTextView.setTextColor(getColor(colorId))
-            descriptionTextView.text = getString(textId)
-        }
+    fun showObesityDialog() {
+        AlertDialog.Builder(this)
+            .setTitle(R.string.obesity_alert_title)
+            .setMessage(R.string.obesity_alert_message)
+            .setPositiveButton(R.string.obesity_alert_button, { dialog, which ->
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.teknon.es/es/especialidades/gonzalbez-morgaez-jose/dietetica-obesidad/recomendaciones-pacientes-obesidad"))
+                startActivity(browserIntent)
+            })
+            .setNegativeButton(android.R.string.cancel, null)
+            .show()
     }
 }
